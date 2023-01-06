53b0d7e056c102d384fb4e43b5c9ac4e4c0b383e
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:14:06 -0600

    set up lopp with control flow

--- divisible_by_ten.py
+++ divisible_by_ten.py
@@ -1,6 +1,8 @@
 #Write your function here
 def divisible_by_ten(nums):
     counter =0
-
+    for num in nums:
+        if num%10==0:
+            counter+=1
 #Uncomment the line below when your function is done
 #print(divisible_by_ten([20, 25, 30, 35, 40]))
