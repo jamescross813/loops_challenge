2ca007db4af8dab2b8410226710554abf34a680b
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:28:45 -0600

    attempt at working func w/loop and control frunc

--- odd_indicies.py
+++ odd_indicies.py
@@ -1,6 +1,12 @@
 #Write your function here
 def odd_indices(lst):
-
+    new_lst =[]
+    index = 0
+    for x in lst:
+        if index %2 != 0:
+            new_lst.append(x)
+        index +=1
+    return new_lst
 
 #Uncomment the line below when your function is done
-#print(odd_indices([4, 3, 7, 10, 11, -2]))
+# print(odd_indices([4, 3, 7, 10, 11, -2]))
