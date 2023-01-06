81cc5209714173b17b48cb4bebe2b683bc72fade
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:24:29 -0600

    attempt at working func

--- delete_starting_even_numbers.py
+++ delete_starting_even_numbers.py
@@ -1,7 +1,10 @@
 #Write your function here
-def delete_starting_even_numbers(lst):
-    
+def delete_starting_evens(lst):
+    while lst[0]%2==0:
+        lst.pop(0)
+    return lst
+
 
 #Uncomment the lines below when your function is done
-#print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
-#print(delete_starting_evens([4, 8, 10]))
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
