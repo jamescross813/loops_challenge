4b371c89db4e00bd158b1c667de041a87e6b18d3
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:14:22 -0600

    return statement

--- divisible_by_ten.py
+++ divisible_by_ten.py
@@ -4,5 +4,6 @@ def divisible_by_ten(nums):
     for num in nums:
         if num%10==0:
             counter+=1
+    return counter
 #Uncomment the line below when your function is done
 #print(divisible_by_ten([20, 25, 30, 35, 40]))
