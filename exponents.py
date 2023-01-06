ae41ab3ff4ed6399d113c5a930ce4114926092c1
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:32:17 -0600

    attempt at working func

--- exponents.py
+++ exponents.py
@@ -1,6 +1,10 @@
 #Write your function here
 def exponents(bases, powers):
-    
+    newlst = []
+    for base in bases:
+        for power in powers:
+            newlst.append(base ** power)
+    return newlst
 
 #Uncomment the line below when your function is done
 #print(exponents([2, 3, 4], [1, 2, 3]))
