26351bb8160ef2c6cd508bc88109f7eec6639a3d
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:18:16 -0600

    attempt at solution

--- add_greetings.py
+++ add_greetings.py
@@ -1,6 +1,8 @@
 #Write your function here
 def add_greetings(names):
-
-
+    lst = []
+    for x in names:
+        lst.append('Hello, '+x)
+    return lst
 #Uncomment the line below when your function is done
 #print(add_greetings(["Owen", "Max", "Sophie"]))
