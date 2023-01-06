88136bb8740b27e1df9f0f2c4db5f35bf499a3fa
jamescross813 <crossjames813@gmail.com>
2023-01-06 14:30:13 -0600

    setup

---
+++ exponents.py
@@ -0,0 +1,5 @@
+#Write your function here
+
+
+#Uncomment the line below when your function is done
+#print(exponents([2, 3, 4], [1, 2, 3]))
--- odd_indicies.py
+++ odd_indicies.py
@@ -9,4 +9,4 @@ def odd_indices(lst):
     return new_lst
 
 #Uncomment the line below when your function is done
-# print(odd_indices([4, 3, 7, 10, 11, -2]))
+print(odd_indices([4, 3, 7, 10, 11, -2]))
