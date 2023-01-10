46da09af0affcc4341cf76fc0f6d5f658eff159c
jamescross813 <crossjames813@gmail.com>
2023-01-09 22:13:39 -0600

    fix typo

--- larger_sum.py
+++ larger_sum.py
@@ -5,10 +5,10 @@ def larger_sum(lst1, lst2):
     for x in lst1:
         sum1+=x
     for y in lst2:
-        sum2+=x
+        sum2+=y
     if sum2>sum1:
         return lst2
     else:
         return lst1
 #Uncomment the line below when your function is done
-#print(larger_sum([1, 9, 5], [2, 3, 7]))
+print(larger_sum([1, 9, 5], [2, 3, 7]))
