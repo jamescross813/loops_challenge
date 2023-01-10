bf285e80610e4acd9ea07e32c88fa9267d9c3616
jamescross813 <crossjames813@gmail.com>
2023-01-09 22:11:20 -0600

    loop lst2

--- larger_sum.py
+++ larger_sum.py
@@ -4,7 +4,8 @@ def larger_sum(lst1, lst2):
     sum2=0
     for x in lst1:
         sum1+=x
-    
+    for y in lst2:
+        sum2+=x
 
 #Uncomment the line below when your function is done
 #print(larger_sum([1, 9, 5], [2, 3, 7]))
