f069dd39751db3df01d1f2b29bc094ecadb1db74
jamescross813 <crossjames813@gmail.com>
2023-01-09 22:12:09 -0600

    set up control flow

--- larger_sum.py
+++ larger_sum.py
@@ -6,6 +6,9 @@ def larger_sum(lst1, lst2):
         sum1+=x
     for y in lst2:
         sum2+=x
-
+    if sum2>sum1:
+        return lst2
+    else:
+        return lst1
 #Uncomment the line below when your function is done
 #print(larger_sum([1, 9, 5], [2, 3, 7]))
