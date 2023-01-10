86ea9ea107498680d8eb17d5f3afdc9617c440ec
jamescross813 <crossjames813@gmail.com>
2023-01-09 22:10:54 -0600

    lopp first list

--- larger_sum.py
+++ larger_sum.py
@@ -2,6 +2,9 @@
 def larger_sum(lst1, lst2):
     sum1=0
     sum2=0
+    for x in lst1:
+        sum1+=x
+    
 
 #Uncomment the line below when your function is done
 #print(larger_sum([1, 9, 5], [2, 3, 7]))
