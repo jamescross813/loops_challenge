5a0248886bffa2df40c0a36814a6f5f6f4b0b353
jamescross813 <crossjames813@gmail.com>
2023-01-10 21:58:03 -0600

    return statement

--- over_nine_thousand.py
+++ over_nine_thousand.py
@@ -6,6 +6,6 @@ def over_nine_thousand(lst):
             sum +=x
         else:
             break
-    
+    return sum
 #Uncomment the line below when your function is done
 # print(over_nine_thousand([8000, 900, 120, 5000]))
