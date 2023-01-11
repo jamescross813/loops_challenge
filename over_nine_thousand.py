38b0aa681debf32b86c5245248c78478768bbe42
jamescross813 <crossjames813@gmail.com>
2023-01-10 21:57:35 -0600

    set up contorl flow

--- over_nine_thousand.py
+++ over_nine_thousand.py
@@ -2,6 +2,10 @@
 def over_nine_thousand(lst):
     sum = 0
     for x in lst:
-        
+        if sum <= 9000:
+            sum +=x
+        else:
+            break
+    
 #Uncomment the line below when your function is done
 # print(over_nine_thousand([8000, 900, 120, 5000]))
