ca24a66ed6509542e632c52effda752436e2b7f5
jamescross813 <crossjames813@gmail.com>
2023-01-10 21:55:03 -0600

    vr sum set up

--- over_nine_thousand.py
+++ over_nine_thousand.py
@@ -1,6 +1,6 @@
 #Write your function here
 def over_nine_thousand(lst):
-    
+    sum = 0
 
 #Uncomment the line below when your function is done
 #print(over_nine_thousand([8000, 900, 120, 5000]))
