971e0dedd54c100c17c1981123852e1fe292e8c0
jamescross813 <crossjames813@gmail.com>
2023-01-10 21:57:16 -0600

    set up loop

--- over_nine_thousand.py
+++ over_nine_thousand.py
@@ -1,6 +1,7 @@
 #Write your function here
 def over_nine_thousand(lst):
     sum = 0
-
+    for x in lst:
+        
 #Uncomment the line below when your function is done
-#print(over_nine_thousand([8000, 900, 120, 5000]))
+# print(over_nine_thousand([8000, 900, 120, 5000]))
