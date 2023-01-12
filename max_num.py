b5a66b7ef4da3f0867fcf9bd5f28fbf9e8646bf4
jamescross813 <crossjames813@gmail.com>
2023-01-11 23:20:30 -0600

    set up loop and control flow within

--- max_num.py
+++ max_num.py
@@ -1,5 +1,8 @@
 #Write your function here
 def max_num(nums):
     max_num = nums[0]
+    for x in nums:
+        if x > max_num:
+            max_num = x
 #Uncomment the line below when your function is done
 #print(max_num([50, -10, 0, 75, 20]))
