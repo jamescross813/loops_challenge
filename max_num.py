bec82b314b33e8d70a2f7c7e570cf6087b30ffc0
jamescross813 <crossjames813@gmail.com>
2023-01-11 23:21:34 -0600

    done

--- max_num.py
+++ max_num.py
@@ -6,4 +6,4 @@ def max_num(nums):
             max_num = x
     return max_num
 #Uncomment the line below when your function is done
-#print(max_num([50, -10, 0, 75, 20]))
+print(max_num([50, -10, 0, 75, 20]))
