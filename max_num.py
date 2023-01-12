107a3476500377f7e72d5e4ed76ca9e19c70ca69
jamescross813 <crossjames813@gmail.com>
2023-01-11 23:20:56 -0600

    return statement added

--- max_num.py
+++ max_num.py
@@ -4,5 +4,6 @@ def max_num(nums):
     for x in nums:
         if x > max_num:
             max_num = x
+    return max_num
 #Uncomment the line below when your function is done
 #print(max_num([50, -10, 0, 75, 20]))
