da4a1cf57a7fe138551e62913daf571b123c7371
jamescross813 <crossjames813@gmail.com>
2023-01-13 23:01:30 -0600

    attempt at working func

--- same_value.py
+++ same_value.py
@@ -1,7 +1,10 @@
 #Write your function here
 def same_values(lst1, lst2):
     match_list = []
-
-
+    for x in lst1:
+        for y in lst2:
+            if x==y:
+                match_list.append
+    return match_list
 #Uncomment the line below when your function is done
 #print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
